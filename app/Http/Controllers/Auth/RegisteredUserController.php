ca659cc3b13a765f3986a2064366041a32bce1f8
Muhammad Mokhtar <120654723+MuhdMokhtar@users.noreply.github.com>
2024-05-17 19:44:13 +0800

    Add: User Role Add: Admin Dashboard Page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $user->assignRole('parent');
 
         event(new Registered($user));
 
