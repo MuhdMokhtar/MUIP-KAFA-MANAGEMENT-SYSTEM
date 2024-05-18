868648a21f19937a0f045e6a12ca489e04d75e63
Muhammad Mokhtar <120654723+MuhdMokhtar@users.noreply.github.com>
2024-05-18 14:22:55 +0800

    update: parents sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,22 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="{{ URL::asset('css/sidebar.css')}}" />
+
+
+    <style>
+        .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #0e2238;
+            color: white;
+            text-align: center;
+        }
+    </style>
 
 </head>
 
@@ -31,7 +45,7 @@
                         <i class="lni lni-grid-alt"></i>
                     </button>
                     <div class="sidebar-logo">
-                        <a href="#">KAFA</a>
+                        <a href="#">E-KAFA</a>
                     </div>
                 </div>
                 <ul class="sidebar-nav">
@@ -39,51 +53,22 @@
 
                         <a href="#" class="sidebar-link">
                             <i class="lni lni-cog"></i>
-                            <span>Settings</span>
+                            <span>KAFA ACTIVITIES</span>
                         </a>
 
                     </li>
                     <li class="sidebar-item">
                         <a href="#" class="sidebar-link">
                             <i class="lni lni-agenda"></i>
-                            <span>Task</span>
+                            <span>STUDENT RESULTS</span>
                         </a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#auth" aria-expanded="false" aria-controls="auth">
-                            <i class="lni lni-protection"></i>
-                            <span>Auth</span>
+                        <a href="#" class="sidebar-link">
+                            <i class="bi bi-receipt"></i>
+                            <span>FEES</span>
                         </a>
-                        <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link">Login</a>
-                            </li>
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link">Register</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="sidebar-item">
-                        <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
-                            <i class="lni lni-layout"></i>
-                            <span>Multi Level</span>
-                        </a>
-                        <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                            <li class="sidebar-item">
-                                <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
-                                    Two Links
-                                </a>
-                                <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
-                                    <li class="sidebar-item">
-                                        <a href="#" class="sidebar-link">Link 1</a>
-                                    </li>
-                                    <li class="sidebar-item">
-                                        <a href="#" class="sidebar-link">Link 2</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
+
                     <li class="sidebar-item">
                         <a href="#" class="sidebar-link">
                             <i class="lni lni-popup"></i>
@@ -92,23 +77,24 @@
                     </li>
 
                     <li class="sidebar-item">
-
-                        <a href="{{ route('profile.edit') }}" class="sidebar-link">
-                            <i class="lni lni-user"></i>
-                            <span>Profile</span>
+                        <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#settings" aria-expanded="false" aria-controls="settings">
+                            <i class="lni lni-protection"></i>
+                            <span>Settings</span>
                         </a>
+                        <ul id="settings" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href=" {{ route('profile.edit') }}" class="sidebar-link"><i class="bi bi-person-circle"></i>Profile</a>
+                            </li>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <li class="sidebar-item">
+                                    <a href="route('logout')" onclick="event.preventDefault();
+                                                this.closest('form').submit();" class="sidebar-link"><i class="lni lni-exit"></i>Logout</a>
+                                </li>
+                            </form>
+                        </ul>
                     </li>
-                </ul>
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <div class="sidebar-footer">
-                        <a href="href=" route('logout')" onclick="event.preventDefault();
-                                                this.closest('form').submit();" class="sidebar-link">
-                            <i class="lni lni-exit"></i>
-                            <span>Logout</span>
-                        </a>
-                    </div>
+
             </aside>
             <!-- Page Content -->
             <div class="main p-3">
@@ -116,16 +102,19 @@
 
                     {{ $slot }}
 
-
                 </div>
             </div>
         </div>
 
-
-
-        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
         <script src="{{ URL::asset('js/sidebarscript.js')}}"></script>
+        <footer>
+            <div class="footer">
+                <p>© 2024 E-KAFA. All Rights Reserved</p>
+
+            </div>
+        </footer>
     </body>
+
 
 </html>
