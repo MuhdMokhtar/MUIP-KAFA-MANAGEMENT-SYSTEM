b35d530013bf4d1cefb5e8f43dc0a28f9bbebfc7
Adamz99s <adamyussof.marzuki@gmail.com>
2024-06-01 01:33:22 +0800

    Added swiper js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,6 +22,7 @@
 
        
     </head>
+    <body>
     <nav class="navbar navbar-expand-lg bgcolor">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">KAFA.COM</a>
@@ -76,61 +77,49 @@
 </nav>
 
 <!--MAIN BODY START HERE-->
-<div class="container-full">
-            <section class="w-100 vh-100 d-flex flex-column justify-content-center align-items-center wrapper-main ">
-                <!-- Slider main container -->
-                <div class="swiper">
-                <!-- Additional required wrapper -->
-                <div class="swiper-wrapper">
-                    <!-- Slides -->
-                    <div class="swiper-slide"></div>
-                    <div class="swiper-slide">Slide 2</div>
-                    <div class="swiper-slide">Slide 3</div>
-                    
-                </div>
-                <!-- If we need pagination -->
-                <div class="swiper-pagination"></div>
+   <!-- Slider main container -->
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    ...
+  </div>
 
-                <!-- If we need navigation buttons -->
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
 
-                <!-- If we need scrollbar -->
-                <div class="swiper-scrollbar"></div>
-                </div>
-                <script>
-                    const swiper = new Swiper('.swiper', {
-                    // Optional parameters
-                    direction: 'horizontal',
-                    loop: true,
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
 
-                    // If we need pagination
-                    pagination: {
-                        el: '.swiper-pagination',
-                    },
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
 
-                    // Navigation arrows
-                    navigation: {
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    },
+<script>
+    const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-                    // And if we need scrollbar
-                    scrollbar: {
-                        el: '.swiper-scrollbar',
-                    },
-                    });
-                </script>
-                <!--
-              <h1 class="centered-text typewriter" style="font-family: Chewy;">Welcome to Kafa.com</h1>
-                -->
-              <br>
-              <div class=" container">
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-              </div>
-            </section>
-          </div>
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+</script>
           
     </body>
 </html>
